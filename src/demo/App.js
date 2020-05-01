bf0b04558eea93999de4f38cf789a3dd40ed5db5
Kevin Castejon <contact@kevincastejon.fr>
2020-05-01 17:48:53 +0200

    fixed repo url

--- src/demo/App.js
+++ src/demo/App.js
@@ -8,7 +8,7 @@ const App = () => {
   <div style={styles.root}>
   <h1>@kevincastejon/react-skill-bars live demo</h1>
     <SkillBars skills={skills} />
-    <a style={{color: 'white'}} href="https://github.com/kevincastejon/@kevincastejon/react-skill-bars">GITHUB sources</a>
+    <a style={{color: 'white'}} href="https://github.com/kevincastejon/react-skill-bars">GITHUB sources</a>
   </div>
 )};
 
