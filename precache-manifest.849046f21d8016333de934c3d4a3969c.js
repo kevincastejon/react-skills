248f5ddd21a1ae45e8da799cd74181b3cbc9a481
Kevin Castejon <contact@kevincastejon.fr>
2020-05-06 06:40:25 +0200

    Updates

---
+++ precache-manifest.849046f21d8016333de934c3d4a3969c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8afab6a94f147e3731d186c3a83e8f0a",
+    "url": "/react-skills/index.html"
+  },
+  {
+    "revision": "9beffaae3eeb0ad6b467",
+    "url": "/react-skills/static/css/main.3cee8a0e.chunk.css"
+  },
+  {
+    "revision": "288fe18085182e1417e6",
+    "url": "/react-skills/static/js/2.b197bd58.chunk.js"
+  },
+  {
+    "revision": "9beffaae3eeb0ad6b467",
+    "url": "/react-skills/static/js/main.1cd9534f.chunk.js"
+  },
+  {
+    "revision": "65b0f489124e8f6f69b0",
+    "url": "/react-skills/static/js/runtime-main.44480c21.js"
+  }
+]);
