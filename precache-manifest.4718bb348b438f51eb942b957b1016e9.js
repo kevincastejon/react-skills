6475547859c4268b9f4bb6526d80bd2773c040b6
Kevin Castejon <contact@kevincastejon.fr>
2020-05-08 19:30:24 +0200

    Updates

---
+++ precache-manifest.4718bb348b438f51eb942b957b1016e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbd598071ca8ae865d5999728d152f2",
+    "url": "/react-skills/index.html"
+  },
+  {
+    "revision": "53aba5c6f7dad7644f8a",
+    "url": "/react-skills/static/css/main.3cee8a0e.chunk.css"
+  },
+  {
+    "revision": "aa3b930bb00e6a1ff224",
+    "url": "/react-skills/static/js/2.b67e5941.chunk.js"
+  },
+  {
+    "revision": "53aba5c6f7dad7644f8a",
+    "url": "/react-skills/static/js/main.8a7b94cc.chunk.js"
+  },
+  {
+    "revision": "65b0f489124e8f6f69b0",
+    "url": "/react-skills/static/js/runtime-main.44480c21.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "react-skills/precache-manifest.9bb20ad8fb8f3f6ce517d8251e09e319.js"
+  "/react-skills/precache-manifest.4718bb348b438f51eb942b957b1016e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("react-skills/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-skills/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
